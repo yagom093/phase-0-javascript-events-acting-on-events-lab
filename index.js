aebf05530b851c99bfb9582387d41f4d30a61202
yago <yagom10@hotmail.com>
2023-04-21 12:39:44 -0700

    complete

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Your code here
+// Move Dodger right code
+function moveDodgerLeft() {
+    const leftNumbers= dodger.style.left.replace("px","")
+    const left = parseInt(leftNumbers, 10)
+    if(left < 350) {
+        dodger.style.left = `${left - 2}px`
+    }
+}
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+        moveDodgerRight()
+    }
+})
+
+
+function moveDodgerRight() {
+    const leftNumbers= dodger.style.left.replace("px","")
+    const left = parseInt(leftNumbers, 10)
+    if(left < 360) {
+        dodger.style.left = `${left + 1}px`
+    }
+}
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+        moveDodgerRight()
+    }
+})
